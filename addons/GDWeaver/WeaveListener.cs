11f85bcc593dd6eb9430ef34624018b812f72c48
Clay Paris <eiffeldud@gmail.com>
2019-04-07 19:55:02 -0700

    Fix so that it works with latest gd-weaver.

--- addons/GDWeaver/WeaveListener.cs
+++ addons/GDWeaver/WeaveListener.cs
@@ -1,27 +1,13 @@
 using Godot;
-using Atko.GDWeaver.Weaving;
+using Atko.GDWeaver;
 
 namespace Atko.GDWeaver.Assets
 {
-    /// <summary>
-    /// Node which, when present in the scene tree, will automatically call <see cref="Weaver.Weave"/> on nodes as soon
-    /// as they are added to the tree.
-    /// </summary>
-    public class WeaveListener : Node
-    {
-        internal WeaveListener()
-        {
-            Name = nameof(WeaveListener);
-        }
-
-        public override void _Ready()
-        {
-            GetTree().Connect("node_added", this, nameof(WeaveListener));
-        }
-
-        void Weave(Node node)
-        {
-            Weaver.Weave(node);
-        }
-    }
+	/// <summary>
+	/// Node which, when present in the scene tree, will automatically call <see cref="Weaver.Weave"/> on nodes as soon
+	/// as they are added to the tree.
+	/// </summary>
+	public class WeaveListener : AutoWeaverBase
+	{
+	}
 }
